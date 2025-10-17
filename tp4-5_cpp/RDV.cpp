5626b77c037995960dac7d63c9ff4332b52d7f1e
gui2L <gsurleau@gmail.com>
2025-10-17 19:26:03 +0200

    add attributes & methods signature

--- tp4-5_cpp/RDV.cpp
+++ tp4-5_cpp/RDV.cpp
@@ -1,1 +1,2 @@
 #include "RDV.h"
+#include <iostream>
--- tp4-5_cpp/RDV.h
+++ tp4-5_cpp/RDV.h
@@ -1,7 +1,32 @@
+#include "Date.h"
+#include "Heure.h"
+#include <iostream>
+#include <vector>
+
+
 #ifndef RDV_H
 #define RDV_H
 
 class RDV {
+    private:
+        Date d;
+        Heure h;
+        std::string lieu;
+        int nb_participants;
+        std::string *participants;
+
+    public:
+        RDV(int nb_particpants = 0, std::string *ps = new std::string[10]);
+        void affiche();
+        void saisieParticipants();
+        void saisieLieu(); 
+        void saisie(); 
+        void setDate(const Date& dateRdv);  
+        void setHeure(const Heure& heureRdv);  
+        void setLieu(const std::string& lieuRdv);  
+        void setNb_participants(int nb_participants); 
+        void setParticipants(std::string* ps);  
+        void setParticipant(int i, std::string s);
 };
 
 #endif
