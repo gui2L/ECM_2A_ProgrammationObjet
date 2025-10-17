76a5682d56344f8bf831083a4c74db86772b39d3
gui2L <gsurleau@gmail.com>
2025-10-17 22:35:16 +0200

    add method saisie

--- tp4-5_cpp/RDV.cpp
+++ tp4-5_cpp/RDV.cpp
@@ -1,5 +1,6 @@
 #include "RDV.h"
 #include <iostream>
+#include <sstream>
 
 
 RDV::RDV() : d(), h(){
@@ -37,4 +38,51 @@ void RDV::saisieLieu(){
     std::cout<< "rentrez le lieu du rdv :";
     std::cin>> l;
     lieu = l;
+}
+
+void RDV::saisie(){
+
+    saisieLieu();
+
+    std::string date;
+    int jour, mois, annee;
+    char sep; 
+    while (true){
+        std::cout<< "rentrez la date du rdv sous format jj/mm/aa :";
+        std::cin>> date;
+        std::stringstream ss(date);
+        ss >> jour >> sep >> mois >> sep >> annee;
+        if (ss.fail()) {
+            std::cout << "Format de date invalide, veuillez reessayer " << "\n";
+        } else {
+            Date d(jour, mois, annee);
+            setDate(d);
+            break;
+        }
+    }
+    
+    std::string heure;
+    int heures, minutes;
+    while (true){   
+        std::cout<< "rentrez l'heure du rdv sous format XX:XX :";
+        std::cin>> heure;
+        std::stringstream ss(heure);
+        ss >> heures >> sep >> minutes;
+        if (ss.fail()) {
+            std::cout << "Format de l'heure invalide, veuillez reessayer " << "\n";
+        } else {
+            Heure h(heures, minutes);
+            setHeure(h);
+            break;
+        }
+    }
+
+    int nb_p;
+    std::cout<< "rentrez le nombre de participants :";
+    std::cin>> nb_p;
+    setNb_participants(nb_p);
+
+    saisieParticipants();
+    
+
 }
