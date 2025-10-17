e5b53b49f81b5261830d52f62e2e1ca8ba85ec89
gui2L <gsurleau@gmail.com>
2025-10-17 19:09:05 +0200

    better tests with assert

--- tp4-5_cpp/main.cpp
+++ tp4-5_cpp/main.cpp
@@ -18,17 +18,18 @@ int main(){
 
     Date d4(22, 11, 1963);
     std::cout<<"date d4 avant modif: ";d4.affiche();
-    std::cout<<"jour de d4 : " << d4.getJour() << "\n";
-    std::cout<<"mois de d4 : " << d4.getMois() << "\n";
-    std::cout<<"annee de d4 : " << d4.getAnnee() << "\n";
+    std::cout<<"jour de d4 : " << d4.getJour() << "\n"; assert(d4.getJour() == 1);
+    std::cout<<"mois de d4 : " << d4.getMois() << "\n"; assert(d4.getMois() == 1);
+    std::cout<<"annee de d4 : " << d4.getAnnee() << "\n"; assert(d4.getAnnee() == 2000);
+    
 
     d4.setJour(8);
     d4.setMois(11);
-    d4.setAnnee(1960);
+    d4.setAnnee(2002);
     std::cout<<"date d4 apres modif: ";d4.affiche();
-    std::cout<<"jour de d4 : " << d4.getJour() << "\n";
-    std::cout<<"mois de d4 : " << d4.getMois() << "\n";
-    std::cout<<"annee de d4 : " << d4.getAnnee() << "\n";
+    std::cout<<"jour de d4 : " << d4.getJour() << "\n"; assert(d4.getJour() == 8);
+    std::cout<<"mois de d4 : " << d4.getMois() << "\n"; assert(d4.getMois() == 11);
+    std::cout<<"annee de d4 : " << d4.getAnnee() << "\n"; assert(d4.getAnnee() == 2002);
 
     std::cout<<"\n";
 
@@ -40,17 +41,17 @@ int main(){
     std::cout<<"heure h3 : "; h3.affiche();
 
     std::cout<<"\n";
-    
+
     Heure h4(12, 15);
     std::cout<<"heure h4 avant modif: "; h4.affiche();
-    std::cout<<"heures de h4 : " << h4.getHeures() << "\n"; 
-    std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n";
+    std::cout<<"heures de h4 : " << h4.getHeures() << "\n"; assert(h4.getHeures() == 12);
+    std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n"; assert(h4.getMinutes() == 15);
 
     h4.setHeures(14);
     h4.setMinutes(0);
     std::cout<<"heure h4 apres modif: "; h4.affiche();
-    std::cout<<"heures de h4 : " << h4.getHeures() << "\n"; 
-    std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n";
+    std::cout<<"heures de h4 : " << h4.getHeures() << "\n"; assert(h4.getHeures() == 14);
+    std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n"; assert(h4.getMinutes() == 0);
     
 
 
