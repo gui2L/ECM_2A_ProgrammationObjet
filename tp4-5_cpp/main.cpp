1ec36cb612f053e611cc5e6d272a3cb61c0af794
gui2L <gsurleau@gmail.com>
2025-10-18 16:29:54 +0200

    update tests method estCompatibleAvec class RDV

--- tp4-5_cpp/main.cpp
+++ tp4-5_cpp/main.cpp
@@ -11,7 +11,7 @@
 int main(){
 
     //tests class Date
-    /*Date d1(0, 0, 0);
+    Date d1(0, 0, 0);
     Date d2(17, 10, 2025);
     Date d3 = d2;
     std::cout<<"date d1 : ";d1.affiche();
@@ -21,42 +21,42 @@ int main(){
     std::cout<<"\n";
 
     Date d4(22, 11, 1963);
-    std::cout<<"date d4 avant modif: ";d4.affiche();
-    std::cout<<"jour de d4 : " << d4.getJour() << "\n"; assert(d4.getJour() == 1);
-    std::cout<<"mois de d4 : " << d4.getMois() << "\n"; assert(d4.getMois() == 1);
-    std::cout<<"annee de d4 : " << d4.getAnnee() << "\n"; assert(d4.getAnnee() == 2000);
+    //std::cout<<"date d4 avant modif: ";d4.affiche();
+    /*std::cout<<"jour de d4 : " << d4.getJour() << "\n";*/ assert(d4.getJour() == 1);
+    /*std::cout<<"mois de d4 : " << d4.getMois() << "\n";*/ assert(d4.getMois() == 1);
+    /*std::cout<<"annee de d4 : " << d4.getAnnee() << "\n";*/ assert(d4.getAnnee() == 2000);
     
 
     d4.setJour(8);
     d4.setMois(11);
     d4.setAnnee(2002);
-    std::cout<<"date d4 apres modif: ";d4.affiche();
-    std::cout<<"jour de d4 : " << d4.getJour() << "\n"; assert(d4.getJour() == 8);
-    std::cout<<"mois de d4 : " << d4.getMois() << "\n"; assert(d4.getMois() == 11);
-    std::cout<<"annee de d4 : " << d4.getAnnee() << "\n"; assert(d4.getAnnee() == 2002);*/
+    //std::cout<<"date d4 apres modif: ";d4.affiche();
+    /*std::cout<<"jour de d4 : " << d4.getJour() << "\n";*/ assert(d4.getJour() == 8);
+    /*std::cout<<"mois de d4 : " << d4.getMois() << "\n";*/ assert(d4.getMois() == 11);
+    /*std::cout<<"annee de d4 : " << d4.getAnnee() << "\n";*/ assert(d4.getAnnee() == 2002);
 
     std::cout<<"\n";
 
     //tests class Heure
-    /*Heure h1;
+    Heure h1;
     Heure h2(1);
     Heure h3(23, 17);
     std::cout<<"heure h1 : "; h1.affiche();
     std::cout<<"heure h2 : "; h2.affiche();
     std::cout<<"heure h3 : "; h3.affiche();
 
-    std::cout<<"\n";
+    //std::cout<<"\n";
 
     Heure h4(12, 15);
-    std::cout<<"heure h4 avant modif: "; h4.affiche();
-    std::cout<<"heures de h4 : " << h4.getHeures() << "\n"; assert(h4.getHeures() == 12);
-    std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n"; assert(h4.getMinutes() == 15);
+    //std::cout<<"heure h4 avant modif: "; h4.affiche();
+    /*std::cout<<"heures de h4 : " << h4.getHeures() << "\n";*/ assert(h4.getHeures() == 12);
+    /*std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n";*/ assert(h4.getMinutes() == 15);
 
     h4.setHeures(14);
     h4.setMinutes(0);
-    std::cout<<"heure h4 apres modif: "; h4.affiche();
-    std::cout<<"heures de h4 : " << h4.getHeures() << "\n"; assert(h4.getHeures() == 14);
-    std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n"; assert(h4.getMinutes() == 0);*/
+    //std::cout<<"heure h4 apres modif: "; h4.affiche();
+    /*std::cout<<"heures de h4 : " << h4.getHeures() << "\n";*/ assert(h4.getHeures() == 14);
+    /*std::cout<<"minutes de h4 : " << h4.getMinutes() << "\n";*/ assert(h4.getMinutes() == 0);
     
     std::cout<<"\n";
 
@@ -73,23 +73,10 @@ int main(){
     RDV r2; 
     RDV r3; 
 
-    // test1 : r2 compatible avec r3 ?
+    // test1 : r2 compatible avec r3 ? 
+    std::cout<< "--------- cas rdvs meme date, meme heure --------- \n";
     r2.setDate(d_rdv1); r2.setHeure(h_rdv1);
     r3.setDate(d_rdv1); r3.setHeure(h_rdv1);
-
-    r2.affiche();
-    r3.affiche();
-
-    if (r2.estCompatibleAvec(r3)){
-        std::cout << " rdvs compatibles \n";
-    }else{
-        std::cout << " rdvs non compatibles \n";
-    }
-
-    std::cout<<"\n";
-
-    // test2 : r2 compatible avec r3 ?
-    r3.setDate(d_rdv2); r3.setHeure(h_rdv2);
 
     r2.affiche();
     r3.affiche();
@@ -100,5 +87,32 @@ int main(){
         std::cout << "rdvs non compatibles \n";
     }
 
+    std::cout<<"\n";
+
+    // test2 : r2 compatible avec r3 ? 
+    std::cout << "--------- cas rdvs meme date, heure differente --------- \n";
+    r3.setHeure(h_rdv2);
+
+    r2.affiche();
+    r3.affiche();
+
+    if (r2.estCompatibleAvec(r3)){
+        std::cout << "rdvs compatibles \n";
+    }else{
+        std::cout << "rdvs non compatibles \n";
+    }
+
+    // test3 : r2 compatible avec r3 ? 
+    std::cout<< "--------- cas rdvs date et heure differentes --------- \n";
+    r3.setDate(d_rdv2); r3.setHeure(h_rdv2);
+
+    r2.affiche();
+    r3.affiche();
+
+    if (r2.estCompatibleAvec(r3)){
+        std::cout << "rdvs compatibles \n";
+    }else{
+        std::cout << "rdvs non compatibles \n";
+    }
     return 0;
 }
