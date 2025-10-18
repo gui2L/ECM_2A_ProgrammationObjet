6a27deb23f26ea81257fec8709079b60b59acb03
gui2L <gsurleau@gmail.com>
2025-10-18 18:20:09 +0200

    feat: class Agenda

---
+++ tp4-5_cpp/Agenda.h
@@ -0,0 +1,22 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+#include "NoeudRDV.h"
+#include <iostream>
+
+class Agenda {
+private:
+    NoeudRDV *premier; 
+
+public:
+    
+    Agenda();                       
+    Agenda(const Agenda &a);   
+    ~Agenda();                  
+
+    void ajoute(const RDV &r);    
+    void enleve();                  
+    void affiche();      
+};
+
+#endif
