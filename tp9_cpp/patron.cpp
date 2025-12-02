a5c30159e1e3ff10c841aab7a79754c9a2161028
gui2L <gsurleau@gmail.com>
2025-12-02 14:54:08 +0100

    feat:test function somme

--- tp9_cpp/patron.cpp
+++ tp9_cpp/patron.cpp
@@ -14,9 +14,10 @@ T somme(int nb_ele, T tab[]){
 int main(){
     int entiers[] = {1, 2, 3, 4, 5};
     double doubles[] = {1.5, 2.5, 3.0};
-    char caractere[] = {'a', 'b', 'c'};
-    cout <<sizeof(entiers);
-    //cout << somme(sizeof(entiers), entiers);
+    string caracteres[] = {"a", "b", "c"};
+    cout << "somme entiers : " << somme(sizeof(entiers)/sizeof(entiers[0]), entiers) <<"\n";
+    cout << "somme doubles : " << somme(sizeof(doubles)/sizeof(doubles[0]), doubles) <<"\n";
+    cout << "somme caracteres : " << (somme(sizeof(caracteres)/sizeof(caracteres[0]), caracteres)) <<"\n";
 
     return 0;
 }
