e5f4494b2773834a4afac5ba99566e4229ca64fc
gui2L <gsurleau@gmail.com>
2025-12-02 15:51:25 +0100

    feat: add function minimum for 2 tabs + tests

--- tp9_cpp/patron.cpp
+++ tp9_cpp/patron.cpp
@@ -23,9 +23,20 @@ int minimum(int nb_ele, T tab[]){
     return indice;
 }
 
+template <typename T, typename U>
+int minimum_2tab(int nb_ele1, int nb_ele2, T tab1[], U tab2[]){
+    int indice_min1 = minimum(nb_ele1, tab1);
+    int indice_min2 = minimum(nb_ele2, tab2);
+    if (tab1[indice_min1] < tab2[indice_min2]){
+        return indice_min1;
+    }else{
+        return indice_min2;
+    }
+}
+
 int main(){
     int entiers[] = {3, 2, 1, 5, 4}; int t_entiers = sizeof(entiers)/sizeof(entiers[0]);
-    double doubles[] = {3.0, 2.5, 1.5}; int t_doubles = sizeof(doubles)/sizeof(doubles[0]);
+    double doubles[] = {3.0, 1.5, 2.5, 0.2}; int t_doubles = sizeof(doubles)/sizeof(doubles[0]);
     char cars[] = {'a', 'b', 'c'}; int t_cars = sizeof(cars)/sizeof(cars[0]);
     string caracteres[] = {"a", "b", "c"}; int t_caracteres = sizeof(caracteres)/sizeof(caracteres[0]);
     
@@ -40,5 +51,11 @@ int main(){
     string s[] = {"abbbb", "abbab", "aab"};
     int t_s = sizeof(s)/sizeof(s[0]);
     cout << "indice min string : " << minimum(t_s, s)<< "\n";
+
+    cout << "indice min entre doubles et entiers :" << minimum_2tab(t_entiers, t_doubles, entiers, doubles)<< "\n";
+    //cout << int(cars[0]);
+    cout << "indice min entre entiers et cars :" << minimum_2tab(t_entiers, t_cars, entiers, cars)<< "\n";
+
+    //ex4 template que sur la classe
     return 0;
 }
